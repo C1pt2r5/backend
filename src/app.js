2d7fc1ad86a1edf3926b9c5387bb1f64c23178a5
vaibh@Unknown <vaibhavkumar26412@gmail.com>
2025-02-03 23:46:33 +0530

    new file

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ app.use(express.urlencoder({extended:true , limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes
 
+import userRouter from "./routes/user.routes.js"
 
+// route declearation 
+app.use("/api/v1/users",userRouter)
+
+// http://localhost:8000/api/v1/users/register
 export {app}
